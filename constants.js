cabc3248770fe41448ace5a701048d887cf8e254
Raj Raj <raj@cambridgespark.com>
2019-10-26 11:28:34 +0100

    Make skeleton

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const API_ENDPOINT = 'https://758ks8kine.execute-api.eu-west-1.amazonaws.com/production/transport'
---
+++ screens/HomeScreen.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="container">
+    <text class="heading">Home</text>
+    <text class="text">Hello World</text>
+    <button title="View Details" @press="goToTab('Details')"></button>
+    <button title="View Settings" @press="goToTab('Settings')"></button>
+  </view>
+</template>
+
+<style>
+.container {
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  padding: 50px;
+}
+.heading {
+  font-size: 30px;
+  font-weight: bold;
+  color: darkolivegreen;
+  margin: 20px;
+}
+.text {
+  text-align: center;
+  margin: 10px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    navigation: { type: Object }
+  },
+  methods: {
+    goToTab(tab) {
+      this.navigation.navigate(tab)
+    },
+  }
+}
+</script>
